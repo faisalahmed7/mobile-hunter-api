f4063d277893dcbfcb1a5455cf811930cabbdd16
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-03-02 16:13:32 +0600

    OPtional Chaining with single mobile ALl Details

--- js/mobile.js
+++ js/mobile.js
@@ -85,7 +85,25 @@ const mobiledetails = mobile => {
         <h3><strong>Brand:  </strong>${mobile.brand}</h3>
         <h5><strong> ${mobile.name}  </strong> </h5>
         <p><strong>Realease Date: </strong> ${mobile.releaseDate ? mobile.releaseDate : 'Not Found'}  </p>
-      
+        <p><strong>Main Features: </strong></p>
+        <p><strong>Storage: </strong>${mobile.mainFeatures.storage ? mobile.mainFeatures.storage : 'Not Found'}</p>
+        <p><strong>Display Size: </strong> ${mobile.mainFeatures.displaySize ? mobile.mainFeatures.displaySize : 'Not Found'}  </p>
+        <p><strong>Chipset: </strong> ${mobile.mainFeatures.chipSet ? mobile.mainFeatures.chipSet : 'Not Found'}  </p>
+        <p><strong>Memory: </strong> ${mobile.mainFeatures.memory ? mobile.mainFeatures.memory : 'Not Found'}  </p>
+        <p> <strong>Sensors</strong>: 
+        ${mobile.mainFeatures.sensors[0] ? mobile.mainFeatures.sensors[0] : 'Not Found'}, 
+        ${mobile.mainFeatures.sensors[1] ? mobile.mainFeatures.sensors[1] : 'Not Found'},
+        ${mobile.mainFeatures.sensors[2] ? mobile.mainFeatures.sensors[2] : 'Not Found'},
+        ${mobile.mainFeatures.sensors[3] ? mobile.mainFeatures.sensors[3] : 'Not Found'},
+        ${mobile.mainFeatures.sensors[4] ? mobile.mainFeatures.sensors[4] : 'Not Found'},
+        ${mobile.mainFeatures.sensors[5] ? mobile.mainFeatures.sensors[5] : 'Not Found'} </p> 
+        <p><strong>Others: </strong></p>
+        <p><strong>WLAN: </strong> ${mobile?.others?.WLAN ?? ''}  </p>
+        <p><strong>Bluetooth: </strong> ${mobile?.others?.Bluetooth ?? ''}  </p>
+        <p><strong>GPS: </strong> ${mobile?.others?.GPS ?? ''}  </p>
+        <p><strong>NFC: </strong> ${mobile?.others?.NFC ?? ''}  </p>
+        <p><strong>Radio: </strong> ${mobile?.others?.Radio ?? ''}  </p>
+        <p><strong>USB: </strong> ${mobile?.others?.USB ?? ''}  </p>
    
     </div>
    
@@ -93,3 +111,4 @@ const mobiledetails = mobile => {
     mobileDetailDiv.appendChild(div);
 
 }
+
