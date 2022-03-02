7fabcee6b56c12c8cca5fa207fdd9517280a20d8
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-03-02 15:25:03 +0600

    Display All MObiles From API

--- js/mobile.js
+++ js/mobile.js
@@ -3,7 +3,7 @@
 const loadAllMobile = () => {
     const searchArea = document.getElementById('search-area');
     const searchInText = searchArea.value;
-    searchInText.toLowerCase();
+
 
     if (searchInText.length == 0) {
         toggleSpinner('none')
@@ -22,3 +22,39 @@ const loadAllMobile = () => {
     }
 
 }
+
+const showAllMobiles = (mobiles, status) => {
+    if (status == false) {
+
+    }
+    else {
+
+        // console.log(mobiles)
+
+        const mobileDiv = document.getElementById('mobile-container');
+
+        mobileDiv.textContent = '';
+
+        mobiles?.forEach(mobile => {
+            // console.log(mobile)
+            const div = document.createElement('div');
+            div.classList.add('col')
+            div.innerHTML = `
+        <div class="card h-100 mx-3 mt-5">
+                <img src="${mobile.image}" class="card-img-top w-50 mx-auto mt-2" alt="...">
+                <div class="card-body mx-auto">
+                    <h5 class="card-title">${mobile.brand}</h5>
+                    <p class="card-text">${mobile.phone_name}</p>
+                    <a href="#"> <button  onclick="loadMobileDetails('${mobile.slug}')"  class="btn btn-primary ">Details</button></a>
+                </div>
+            </div>
+        `
+            mobileDiv.appendChild(div)
+
+        })
+
+        mobileDetailDiv.textContent = '';
+    }
+
+}
+
